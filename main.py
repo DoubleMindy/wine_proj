6cd98a3d172f63fa23e4301ad97ca681fb02046e
DoubleMindy <doublemind21@gmail.com>
2020-06-26 21:07:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import datetime
 import pprint
+import argparse
 import pandas
 from collections import defaultdict
 
-data_df = pandas.read_excel('wine_full.xlsx').rename(columns = {
+parser = argparse.ArgumentParser(description = 'Шаблонизация винного сайта')
+parser.add_argument('-p', '--path', help = 'Путь к файлу')
+args = parser.parse_args()
+
+data_df = pandas.read_excel(args.path).rename(columns = {
     'Категория': 'wine_type', 
     'Название': 'wine_title', 
     'Сорт': 'wine_sort', 
@@ -14,13 +19,13 @@ data_df = pandas.read_excel('wine_full.xlsx').rename(columns = {
     'Акция': 'wine_sale'
     }).fillna(0)
 
-temp_wine_dict = data_df.to_dict(orient = 'records')
+data_df = data_df.to_dict(orient = 'records')
 
-wine_dict = defaultdict(list)
-for item in temp_wine_dict:
-    wine_dict[item['wine_type']].append(item)
+wines = defaultdict(list)
+for item in data_df:
+    wines[item['wine_type']].append(item)
 
-pprint.pprint(wine_dict, indent = 4)
+# pprint.pprint(wine_dict, indent = 4)
 
 current_year = datetime.datetime.today().year - 1920
 
@@ -32,7 +37,7 @@ env = Environment(
 template = env.get_template('template.html')
 
 rendered_page = template.render(
-    wines = wine_dict,
+    wines = wines,
     cur_year = current_year
 )
 
